c1bba9ffac247845ee0734111364578030ce56b0
Keller Soma <keller.som12@gmail.com>
2023-05-09 13:32:21 +0200

    Add project files.

---
+++ QHI7OE_HFT_2022232.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QHI7OE_HFT_2022232.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ QHI7OE_HFT_2022232.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace QHI7OE_HFT_2022232.Test
+{
+    public class Class1
+    {
+    }
+}
