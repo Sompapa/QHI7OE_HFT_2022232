70d5638c135e8409ddfb95e419558646ca19f717
Keller Soma <keller.som12@gmail.com>
2023-11-20 18:57:31 +0100

    WPF fixed, hub not set

--- QHI7OE_HFT_2022232.Endpoint/Controllers/MangaController.cs
+++ QHI7OE_HFT_2022232.Endpoint/Controllers/MangaController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
+using QHI7OE_HFT_2022232.Endpoint.Services;
 using QHI7OE_HFT_2022232.Logic;
 using QHI7OE_HFT_2022232.Models;
 using System.Collections.Generic;
@@ -11,10 +14,12 @@ namespace QHI7OE_HFT_2022232.Endpoint.Controllers
     {
 
         IMangaLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public MangaController(IMangaLogic logic)
+        public MangaController(IMangaLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -33,18 +38,22 @@ namespace QHI7OE_HFT_2022232.Endpoint.Controllers
         public void Create([FromBody] Manga value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("MangaCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Manga value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("MangaUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var mangaToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("MangaDeleted", mangaToDelete);
         }
     }
 }
---
+++ QHI7OE_HFT_2022232.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using QHI7OE_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QHI7OE_HFT_2022232.WpfClient
+{
+    public  class MainWindowViewModel : ObservableRecipient
+    {
+        //Manga:
+        public RestCollection<Manga> Mangas { get; set; }
+
+        private Manga selectedManga;
+
+        public Manga SelectedManga
+        {
+            get { return selectedManga; }
+            set 
+            {
+                if (value != null)
+                {
+                    selectedManga = new Manga()
+                    {
+                        Title = value.Title,
+                        MangaId = value.MangaId,
+                        Genre = value.Genre,
+                        Author = value.Author,
+                        Rating = value.Rating,
+                        Release = value.Release,
+                        Price = value.Price
+                    };
+                    OnPropertyChanged();
+                    (DeleteMangaCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+                
+            }
+            
+        }
+
+        public ICommand CreateMangaCommand { get; set; }
+        public ICommand DeleteMangaCommand { get; set; }
+        public ICommand UpdateMangaCommand { get; set; }
+
+        //Author:
+        public RestCollection<Author> Authors { get; set; }
+
+        private Author selectedAuthor;
+
+        public Author SelectedAuthor
+        {
+            get { return selectedAuthor; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedAuthor = new Author()
+                    {
+                        AuthorName = value.AuthorName,
+                        AuthorId = value.AuthorId,
+                    };
+                    OnPropertyChanged();
+                    (DeleteAuthorCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+            
+        }
+
+        public ICommand CreateAuthorCommand { get; set; }
+        public ICommand DeleteAuthorCommand { get; set; }
+        public ICommand UpdateAuthorCommand { get; set; }
+
+
+        //Genre:
+        public RestCollection<Genre> Genres { get; set; }
+
+        private Genre selectedGenre;
+
+        public Genre SelectedGenre
+        {
+            get { return selectedGenre; }
+            set 
+            {
+                if (value != null)
+                {
+                    selectedGenre = new Genre()
+                    {
+                        GenreName = value.GenreName,
+                        GenreId = value.GenreId,
+                    };
+                    OnPropertyChanged();
+                    (DeleteGenreCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand CreateGenreCommand { get; set; }
+        public ICommand DeleteGenreCommand { get; set; }
+        public ICommand UpdateGenreCommand { get; set; }
+
+
+        //DesignMode:
+        public static bool IsInDesignMode 
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public MainWindowViewModel()
+        { 
+            //RestCollections:
+            Mangas = new RestCollection<Manga>("http://localhost:59073/", "manga");
+            Authors = new RestCollection<Author>("http://localhost:59073/", "author");
+            Genres = new RestCollection<Genre>("http://localhost:59073/", "genre");
+
+            //Manga Commands:
+            //Create:
+            CreateMangaCommand = new RelayCommand(()=>
+            {
+                Mangas.Add(new Manga()
+                {
+                    Title = SelectedManga.Title,
+                    GenreId = SelectedManga.GenreId,
+                    AuthorId = SelectedManga.AuthorId,
+                    Rating = SelectedManga.Rating,
+                    Release = SelectedManga.Release,
+                    Price = SelectedManga.Price
+                });
+            });
+
+            //Delete:
+            DeleteMangaCommand = new RelayCommand(() =>
+            {
+                Mangas.Delete(SelectedManga.MangaId);
+            },
+            ()=>
+            {
+                return SelectedManga != null;
+            });
+
+            //Update:
+            UpdateMangaCommand = new RelayCommand(() =>
+            {
+                try
+                {
+                    Mangas.Update(SelectedManga);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            });
+
+            SelectedManga = new Manga();
+
+            //Author Commands:
+            //Create:
+            CreateAuthorCommand = new RelayCommand(() =>
+            {
+                Authors.Add(new Author()
+                {
+                    AuthorName = selectedAuthor.AuthorName
+                });
+            });
+
+            //Delete:
+            DeleteAuthorCommand = new RelayCommand(() =>
+            {
+                Authors.Delete(selectedAuthor.AuthorId);
+            },
+            ()=>
+            {
+                return selectedAuthor != null;
+            });
+
+            //Update:
+            UpdateAuthorCommand = new RelayCommand(() =>
+            {
+                try
+                {
+                    Authors.Update(selectedAuthor);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            });
+
+            SelectedAuthor = new Author();
+
+            //Genre Commands:
+            //Create:
+            CreateGenreCommand = new RelayCommand(() =>
+            {
+                Genres.Add(new Genre()
+                {
+                    GenreName=SelectedGenre.GenreName
+                });
+            });
+
+            //Delete:
+            DeleteGenreCommand = new RelayCommand(() =>
+            {
+                Genres.Delete(SelectedGenre.GenreId);
+            },
+            ()=>
+            {
+                return SelectedGenre != null;
+            });
+
+            //Update:
+            UpdateGenreCommand = new RelayCommand(() =>
+            {
+                try
+                {
+                    Genres.Update(SelectedGenre);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            });
+
+            SelectedGenre = new Genre();
+
+        }
+    }
+}
