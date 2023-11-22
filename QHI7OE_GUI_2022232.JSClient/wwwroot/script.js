17bcfb8d2ec205d2db5f2cca52b6b26c4dacc59d
Keller Soma <keller.som12@gmail.com>
2023-11-22 08:04:14 +0100

    Failed fetch Js

--- QHI7OE_GUI_2022232.JSClient/wwwroot/script.js
+++ QHI7OE_GUI_2022232.JSClient/wwwroot/script.js
@@ -1,3 +1,17 @@
-﻿fetch('http://localhost:28642/manga')
+﻿let mangas = [];
+
+fetch('http://localhost:28642/manga')
     .then(x => x.json())
-    .then(y => console.log(y))
+    .then(y => {
+        mangas = y;
+        console.log(mangas);
+        display();
+    });
+
+function display() {
+    actors.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.mangaId + "</td><td>"
+            + t.title + "</td></tr>";
+    });
+}
